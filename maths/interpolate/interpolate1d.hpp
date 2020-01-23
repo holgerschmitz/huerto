4144bafcfa87d414b36d0328185f9e1324caa081
Holger Schmitz <holger@notjustphysics.com>
2020-01-23 08:11:57 +0000

    Factored out interpolation routines

---
+++ maths/interpolate/interpolate1d.hpp
@@ -0,0 +1,41 @@
+/*
+ * interpolate1d.hpp
+ *
+ *  Created on: 22 Jan 2020
+ *  Author: Holger Schmitz
+ */
+
+#ifndef SCHNAR_MATHS_INTERPOLATE_INTERPOLATE1D_HPP_
+#define SCHNAR_MATHS_INTERPOLATE_INTERPOLATE1D_HPP_
+
+#include "../types.hpp"
+
+inline int findInsertIndex(const Grid1d &X, double x)
+{
+  int lo = X.getLo(0);
+  int hi = X.getHi(0);
+
+  while (lo <= hi) {
+      int mid = (hi + lo) / 2;
+
+      if (x < X(mid))
+      {
+          hi = mid - 1;
+      }
+      else
+      {
+          lo = mid + 1;
+      }
+  }
+  return hi;
+}
+
+inline double linearInterpolate(const Grid1d &X, const Grid1d &Y, double x)
+{
+  if (x<=X(X.getLo(0))) return Y(X.getLo(0));
+  if (x>=X(X.getHi(0))) return Y(X.getHi(0));
+  int p = findInsertIndex(X, x);
+  return (Y(p+1) - Y(p))*(x - X(p))/(X(p+1) - X(p)) + Y(p);
+}
+
+#endif /* SCHNAR_MATHS_INTERPOLATE_INTERPOLATE1D_HPP_ */
