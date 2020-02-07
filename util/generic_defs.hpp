a51bd0f034412d113a628146d5bc8703d582302f
Holger Schmitz <holger@notjustphysics.com>
2020-02-07 14:48:27 +0000

    Added Domain types and allowed array expressions dot product

---
+++ util/generic_defs.hpp
@@ -0,0 +1,35 @@
+/*
+ * generic_defs.hpp
+ *
+ *  Created on: 30 Jan 2020
+ *      Author: Holger Schmitz
+ */
+
+#include <schnek/grid/arrayexpression.hpp>
+
+#define SCHNAR_FUNC_EXPR_EXPR(type, func)                                       \
+  template<class exp1, class exp2, int rank>                                    \
+  inline type func (                                                            \
+    const schnek::ArrayExpression<exp1, rank> &A,                             \
+    const schnek::ArrayExpression<exp2, rank> &B)                             \
+  {                                                                             \
+    return func(schnek::Array<type, rank>(A), schnek::Array<type, rank>(B));    \
+  }
+
+#define SCHNAR_FUNC_ARR_EXPR(type, func)                                        \
+  template<class exp, int rank, template<int> class CheckPolicy>                              \
+  inline type func (                                                            \
+    const schnek::ArrayExpression<exp, rank> &A,                              \
+    const schnek::Array<type, rank, CheckPolicy> &B)                          \
+  {                                                                             \
+    return func(schnek::Array<type, rank>(A), B);                               \
+  }
+
+#define SCHNAR_FUNC_EXPR_ARR(type, func)                                        \
+  template<class exp, int rank, template<int> class CheckPolicy>                              \
+  inline type func (                                                            \
+    const schnek::Array<type, rank, CheckPolicy> &A,                           \
+    const schnek::ArrayExpression<exp, rank> &B)                              \
+  {                                                                             \
+    return func(A, schnek::Array<type, rank>(B));                               \
+  }
---
+++ util/generic_undefs.hpp
@@ -0,0 +1,10 @@
+/*
+ * generic_undefs.hpp
+ *
+ *  Created on: 30 Jan 2020
+ *      Author: Holger Schmitz
+ */
+
+#undef SCHNAR_FUNC_EXPR_EXPR
+#undef SCHNAR_FUNC_ARR_EXPR
+#undef SCHNAR_FUNC_EXPR_ARR
