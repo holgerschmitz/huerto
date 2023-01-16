dd9c7503e9aa8c4d1ed345b7d4f5e2890dc9a047
Holger Schmitz <holger@notjustphysics.com>
2023-01-16 18:28:33 +0000

    Added some docu on small object storage

--- storage/small_object_storage.hpp
+++ storage/small_object_storage.hpp
@@ -13,33 +13,46 @@
 #include <fstream>
 
 struct DefaultSmallObjectStorageTraits {
-    static const size_t blockSize = 1000000;
+    static const size_t chunkSize = 1000000;
 };
 
+/**
+ * @brief A dynamic storage for small(ish) objects of equal size.
+ * 
+ * The objects are stored in chunks of a specified size.
+ * The chunks are stored in a STL list.
+ * 
+ * Insertion of elements is performed by writing the element into the next space of
+ * a chunk that contains free spaces. Internally a reference of such a chunk is held.
+ * This means, most insertions are performed in constant time.
+ * 
+ * Provides an STL style iterator and `begin()` and `end()` methods to iterate over all 
+ * elements.
+ */
 template<typename T, class Traits = DefaultSmallObjectStorageTraits>
 class SmallObjectStorage {
   public:
     typedef T value_type;
   private:
-    struct DataBlock {
+    struct DataChunk {
         T *data;
         size_t count;
-        DataBlock() = delete;
-        DataBlock(T *data);
-        DataBlock(const DataBlock &block);
+        DataChunk() = delete;
+        DataChunk(T *data);
+        DataChunk(const DataChunk &chunk);
         T &addItem();
     };
 
-    typedef std::list<DataBlock> BlockList;
-    typedef typename BlockList::iterator BlockIterator;
-    typedef typename BlockList::const_iterator BlockConstIterator;
-    BlockList blocks;
-    BlockIterator freeBlock;
+    typedef std::list<DataChunk> ChunkList;
+    typedef typename ChunkList::iterator ChunkIterator;
+    typedef typename ChunkList::const_iterator ChunkConstIterator;
+    ChunkList chunks;
+    ChunkIterator freeChunk;
   public:
 
     SmallObjectStorage() {
-//      blocks.push_front(DataBlock(new T[Traits::blockSize]));
-      freeBlock = blocks.end();
+//      chunks.push_front(DataChunk(new T[Traits::chunkSize]));
+      freeChunk = chunks.end();
     }
     
     SmallObjectStorage(const SmallObjectStorage&) = delete;
@@ -49,19 +62,19 @@ class SmallObjectStorage {
     class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
       private:
         friend class SmallObjectStorage<T, Traits>;
-        BlockIterator blockIter;
+        ChunkIterator chunkIter;
         size_t pos;
 
-        iterator(BlockIterator blockIter_, long pos_ = 0) :
-          blockIter(blockIter_), pos(pos_) {}
+        iterator(ChunkIterator chunkIter_, long pos_ = 0) :
+          chunkIter(chunkIter_), pos(pos_) {}
       public:
         iterator() : pos(0) {}
 
-        iterator(const iterator &it) : blockIter(it.blockIter), pos(it.pos) {}
+        iterator(const iterator &it) : chunkIter(it.chunkIter), pos(it.pos) {}
 
         iterator& operator++() {
-          if (++pos >= blockIter->count) {
-            ++blockIter;
+          if (++pos >= chunkIter->count) {
+            ++chunkIter;
             pos = 0;
           }
           return *this;
@@ -75,8 +88,8 @@ class SmallObjectStorage {
 
         iterator& operator--() {
           if (--pos < 0) {
-            --blockIter;
-            pos = blockIter->count - 1;
+            --chunkIter;
+            pos = chunkIter->count - 1;
           }
           return *this;
         }
@@ -88,32 +101,32 @@ class SmallObjectStorage {
         }
 
         bool operator==(const iterator& rhs) const {
-          return (blockIter == rhs.blockIter) && (pos == rhs.pos);
+          return (chunkIter == rhs.chunkIter) && (pos == rhs.pos);
         }
 
         bool operator!=(const iterator& rhs) const {
-          return (blockIter != rhs.blockIter) || (pos != rhs.pos);
+          return (chunkIter != rhs.chunkIter) || (pos != rhs.pos);
         }
 
         T& operator*() {
-          return blockIter->data[pos];
+          return chunkIter->data[pos];
         }
 
         T* operator->() {
-          return &(blockIter->data[pos]);
+          return &(chunkIter->data[pos]);
         }
     };
 
     iterator begin() {
-      BlockIterator b = blocks.begin();
-      while (b->count == 0 && b != blocks.end()) {
+      ChunkIterator b = chunks.begin();
+      while (b->count == 0 && b != chunks.end()) {
         ++b;
       }
       return iterator(b);
     }
 
     iterator end() {
-      return iterator(blocks.end());
+      return iterator(chunks.end());
     }
 
     T &addItem();
@@ -137,65 +150,69 @@ class SmallObjectStorage {
     void clear();
 };
 
+//=================================================================
+//=============== SmallObjectStorage ==============================
+//=================================================================
+
 template<typename T, class Traits>
-SmallObjectStorage<T, Traits>::DataBlock::DataBlock(T *data) : data(data), count(0) {
+SmallObjectStorage<T, Traits>::DataChunk::DataChunk(T *data) : data(data), count(0) {
 }
 
 template<typename T, class Traits>
-SmallObjectStorage<T, Traits>::DataBlock::DataBlock(const DataBlock &block) : data(block.data), count(block.count) {
+SmallObjectStorage<T, Traits>::DataChunk::DataChunk(const DataChunk &chunk) : data(chunk.data), count(chunk.count) {
 }
 
 template<typename T, class Traits>
-T &SmallObjectStorage<T, Traits>::DataBlock::addItem() {
+T &SmallObjectStorage<T, Traits>::DataChunk::addItem() {
   return data[count++];
 }
 
 template<typename T, class Traits>
 T &SmallObjectStorage<T, Traits>::addItem() {
-  // find a free block or create one
-  if ((freeBlock == blocks.end()) || (Traits::blockSize == freeBlock->count)) {
-    freeBlock = blocks.begin();
+  // find a free chunk or create one
+  if ((freeChunk == chunks.end()) || (Traits::chunkSize == freeChunk->count)) {
+    freeChunk = chunks.begin();
 
-    while((freeBlock != blocks.end()) && (Traits::blockSize <= freeBlock->count)) {
-      ++freeBlock;
+    while((freeChunk != chunks.end()) && (Traits::chunkSize <= freeChunk->count)) {
+      ++freeChunk;
     }
 
-    if (freeBlock == blocks.end()) {
-      freeBlock = blocks.insert(blocks.begin(), DataBlock(new T[Traits::blockSize]));
+    if (freeChunk == chunks.end()) {
+      freeChunk = chunks.insert(chunks.begin(), DataChunk(new T[Traits::chunkSize]));
     }
   }
-  return freeBlock->addItem();
+  return freeChunk->addItem();
 }
 
 template<typename T, class Traits>
 typename SmallObjectStorage<T, Traits>::iterator SmallObjectStorage<T, Traits>::removeItem(const iterator &it_) {
   iterator it = it_;
-  DataBlock &dblock = *(it.blockIter);
-  T *data = dblock.data;
+  DataChunk &dchunk = *(it.chunkIter);
+  T *data = dchunk.data;
   long pos = it.pos;
-  long last = dblock.count - 1;
+  long last = dchunk.count - 1;
 
   if (last>pos) std::swap(data[pos], data[last]);
 
-  --(dblock.count);
-  // if no more rays in the block then delete the block
-  if (0 == dblock.count && blocks.size() > 1) {
-    std::cerr << "SmallObjectStorage deleting data" << dblock.data << std::endl;
-    delete[] dblock.data;
-    it.blockIter = blocks.erase(it.blockIter);
+  --(dchunk.count);
+  // if no more items in the chunk then delete the chunk
+  if (0 == dchunk.count && chunks.size() > 1) {
+    std::cerr << "SmallObjectStorage deleting data" << dchunk.data << std::endl;
+    delete[] dchunk.data;
+    it.chunkIter = chunks.erase(it.chunkIter);
     it.pos = 0;
-    freeBlock = blocks.begin();
+    freeChunk = chunks.begin();
   } else if (last==pos) {
-    ++it.blockIter;
+    ++it.chunkIter;
     it.pos = 0;
   }
 
-  // for performance, see if there are more free spaces in this block than in the
-  // freeBlock, and update freeBlock accordingly
-//  if ((it.blockIter != blocks.end()) && (it.blockIter->count < freeBlock->count))
-//  {
-//    freeBlock = it.blockIter;
-//  }
+  // for performance, see if there are more free spaces in this chunk than in the
+  // freeChunk, and update freeChunk accordingly
+  //  if ((it.chunkIter != chunks.end()) && (it.chunkIter->count < freeChunk->count))
+  //  {
+  //    freeChunk = it.chunkIter;
+  //  }
   return it;
 }
 
@@ -203,25 +220,26 @@ template<typename T, class Traits>
 long SmallObjectStorage<T, Traits>::getCount() const
 {
   long count = 0;
-  for (BlockConstIterator b = blocks.begin(); b!=blocks.end(); ++b)
+  for (ChunkConstIterator b = chunks.begin(); b!=chunks.end(); ++b)
     count += b->count;
   return count;
 }
 
 template<typename T, class Traits>
 void SmallObjectStorage<T, Traits>::clear() {
-    for (BlockIterator b = blocks.begin(); b!=blocks.end(); ++b) {
+    for (ChunkIterator b = chunks.begin(); b!=chunks.end(); ++b) {
       delete[] b->data;
     }
-    blocks.clear();
-    freeBlock = blocks.end();
+    chunks.clear();
+    freeChunk = chunks.end();
 }
 
 template<typename T, class Traits>
 SmallObjectStorage<T, Traits>::~SmallObjectStorage() {
-    for (BlockIterator b = blocks.begin(); b!=blocks.end(); ++b) {
+    for (ChunkIterator b = chunks.begin(); b!=chunks.end(); ++b) {
       delete[] b->data;
     }
-    blocks.clear();
+    chunks.clear();
 }
+
 #endif /* HUERTO_STORAGE_SMALL_OBJECT_STORAGE_HPP_ */
